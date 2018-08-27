34aba66e7d129b79153aa5eb67719cb58ba09758
MarsomUp <MarsomUp@users.noreply.github.com>
2018-08-27 19:01:57 +0800

    Update Message.java
    tijiao

--- src/com/myc/Message.java
+++ src/com/myc/Message.java
@@ -2,4 +2,16 @@ package com.myc
 
 public class Message {
 	private String id;
+	private String name;
+	private String address;
+	
+	public Message() {}
+	
+	public void setId(String id) {
+	    this.id = id
+	}
+	
+	public String getId() {
+	    return this.id;
+	}
 }
